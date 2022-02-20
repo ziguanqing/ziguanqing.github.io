2caf66ceb3b40963306edce7123bf1bed3638f7c
ziguanqing <2027004326@qq.com>
2022-02-20 18:34:44 +0800

    Site updated: 2022-02-20 18:34:43

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,1 +1,1 @@
-!function(t,e){for(const t of e.querySelectorAll("img[lazyload]"))Fluid.utils.waitElementVisible(t,(function(){t.removeAttribute("srcset"),t.removeAttribute("lazyload")}),CONFIG.lazyload.offset_factor)}(window,document);
+!function(){for(const t of document.querySelectorAll("img[lazyload]"))Fluid.utils.waitElementVisible(t,(function(){t.removeAttribute("srcset"),t.removeAttribute("lazyload")}),CONFIG.lazyload.offset_factor)}(window);
