a56f9eeb43891b1643de2086e8d0a05289e741d5
ziguanqing <2027004326@qq.com>
2022-05-09 12:58:19 +0800

    Site updated: 2022-05-09 12:58:18

--- js/stars.js
+++ js/stars.js
@@ -1,1 +1,40 @@
-stars={init:function(n){var i=this;$.getJSON(n,(function(n){for(var t in n){var d=n[t];i.render(d,t)}}))},render:function(n,i){for(var t,d,a="",r=0;r<n.length;r++)t=n[r].nickname,d=n[r].site,content=n[r].content,a+='<div class="card" onclick="window.open(\''+d+'\')" ><div class="card-header"><div>'+t+'</div></div><div class="card-content"><div>'+content+"</div></div></div>";$(i).append(a)}},stars.init("../allstars.json");
+// 采用一个多对象JSON文件存储所有数据的方式
+stars = {
+  init: function (url) {
+    var that = this
+    $.getJSON(url, function (data) {
+      for (var className in data) {
+        var classData = data[className]
+        that.render(classData, className)
+      }
+    })
+  },
+  render: function (data, name) {
+    var nickname,
+      site,
+      li = ''
+    for (var i = 0; i < data.length; i++) {
+      nickname = data[i].nickname
+      site = data[i].site
+      content = data[i].content
+      li +=
+        '<div class="card" onclick="window.open(\'' +
+        site +
+        '\')" >' +
+        '<div class="card-header">' +
+        '<div>' +
+        nickname +
+        '</div>' +
+        '</div>' +
+        '<div class="card-content">' +
+        '<div>' +
+        content +
+        '</div>' +
+        '</div>' +
+        '</div>'
+    }
+    $(name).append(li)
+  },
+}
+// 传入json文件的路径
+stars.init('../allstars.json')
