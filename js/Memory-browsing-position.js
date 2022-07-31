c6fa31fc43d93f74215597738e83a6a19f51b612
ziguanqing <2027004326@qq.com>
2022-07-31 13:18:55 +0800

    Site updated: 2022-07-31 13:18:55

---
+++ js/Memory-browsing-position.js
@@ -0,0 +1,1 @@
+$((function(){localStorage.tempScrollTop&&$(window).scrollTop(localStorage.tempScrollTop)})),$(window).on("scroll",(function(){localStorage.setItem("tempScrollTop",$(window).scrollTop())})),window.onbeforeunload=function(){var o=$(window).scrollTop();localStorage.setItem("tempScrollTop",o)};
